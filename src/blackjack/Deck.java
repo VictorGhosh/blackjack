6f14c1431878870a46034c4f2e0094bf50f41cdf
Victor <36003172+VictorGhosh@users.noreply.github.com>
2021-04-09 13:42:35 -0400

    Basic deck class
    can now create and shuffle a deck object.

---
+++ src/blackjack/Deck.java
@@ -0,0 +1,56 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This object represents a standard 52 card deck0
+ */
+public class Deck {
+
+	/**
+	 * List of cards in deck
+	 */
+	private List<Card> deck;
+
+	/**
+	 * Initialize a full deck. Cards are in order
+	 */
+	public Deck() {
+		this.deck = new ArrayList<>();
+
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				deck.add(new Card(rank, suit));
+			}
+		}
+	}
+
+	/**
+	 * Shuffle this deck
+	 */
+	public void shuffle() {
+		Collections.shuffle(this.deck);
+	}
+
+	/**
+	 * Pulls the top card from the deck and returns that card
+	 * 
+	 * @return the card that was pulled
+	 */
+	public Card draw() {
+		Card res = this.deck.get(0);
+		this.deck.remove(0);
+		return res;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+		for (Card card : this.deck) {
+			res.append(card + "\n");
+		}
+		return res.toString();
+	}
+}
--- src/blackjack/Rank.java
+++ src/blackjack/Rank.java
@@ -1,8 +1,8 @@
 package blackjack;
 
 /**
- * All ranks for a standard deck of cards in numerical order
+ * All ranks for a standard deck of cards in numerical order with high ace
  */
 public enum Rank {
-	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
