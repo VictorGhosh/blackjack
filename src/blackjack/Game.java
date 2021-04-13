e81f249d3cfb6cbcaac0e12b03f5f4c4f5a2bf8f
Victor <36003172+VictorGhosh@users.noreply.github.com>
2021-04-13 15:29:03 -0400

    Redid some of game

--- src/blackjack/Game.java
+++ src/blackjack/Game.java
@@ -9,7 +9,7 @@ public class Game {
 	 * List of players in the game
 	 */
 	private List<Player> players;
-	
+
 	/**
 	 * Game dealer
 	 */
@@ -40,50 +40,104 @@ public class Game {
 		}
 
 		this.players = new ArrayList<>();
-		for (int i = 0; i < numPlayers + 1; i++) {
+		for (int i = 0; i < numPlayers; i++) {
 			this.players.add(new Player());
 		}
+
+		this.dealer = new Dealer();
 
 		this.deck = new Deck();
 		this.deck.shuffle();
 	}
 
 	/**
-	 * Deals card from deck to specified player. Player must be in the game
+	 * Returns list of players on board. First player is dealer. Note, the getHand()
+	 * function for dealer will not return second card until it has been flipped.
 	 * 
-	 * @param p Player to deal the card to
-	 * @throws IllegalArgumentException if player is not in this game
+	 * @return list of players on board. First player is dealer.
 	 */
-	public void hit(Player p) {
-		if (!this.players.contains(p)) {
+	public List<Player> getBoard() {
+		List<Player> res = new ArrayList<>();
+		res.add(dealer);
+		res.addAll(this.players);
+		return res;
+	}
+
+	/**
+	 * Executes move for player p. Move is a specified int value, 0 for stand 2 for
+	 * hit. Player must be in this game.
+	 * 
+	 * @param p player to make move
+	 * @param move move to make
+	 * @return player that made move
+	 * @throws IllegalArgumentException if player is not in game
+	 * @throws IllegalArgumentException if move is not legal
+	 */
+	public Player turn(Player p, int move) {
+		Player player;
+		if (this.players.contains(p)) {
+			player = this.players.get(this.players.indexOf(p));
+		}
+		else if (p == this.dealer) {
+			player = this.dealer;
+		} else {
 			throw new IllegalArgumentException("Player not in game");
 		}
-		p.take(this.deck.draw());
+		
+		switch (move) {
+		case 0:
+			stand(player);
+			break;
+		case 1:
+			hit(player);
+			break;
+		default:
+			throw new IllegalArgumentException("Illegal move");
+		}
+		return player;
 	}
 
 	/**
 	 * Deal each player and the dealer two cards to start the game
 	 */
-	public void start() {
+	public void deal() {
 		for (int i = 0; i < 2; i++) {
 			for (Player p : this.players) {
 				this.hit(p);
 			}
+			this.hit(dealer);
 		}
 	}
-	
+
+	/**
+	 * Player stands. Returns player
+	 * 
+	 * @param p Player to stand
+	 * @return Player that stood
+	 */
+	public Player stand(Player p) {
+		return p;
+	}
+
+	/**
+	 * Deals card from deck to specified player. Player must be in the game
+	 * 
+	 * @param p Player to deal the card to
+	 * @return The player that was hit
+	 * @throws IllegalArgumentException if player is not in this game
+	 */
+	public Player hit(Player p) {
+		p.take(this.deck.draw());
+		return p;
+	}
 
 	@Override
 	public String toString() {
 		StringBuilder res = new StringBuilder();
+		res.append("Dealer:\n" + this.dealer);
 		for (Player p : this.players) {
-			int index = this.players.indexOf(p);
-			if (index == 0) {
-				res.append("Dealer:\n");
-			} else {
-				res.append("\nPlayer " + index + "\n");
-			}
-			res.append(p);
+			int index = this.players.indexOf(p) + 1;
+			res.append("\nPlayer " + index + ":\n" + p);
 		}
 		return res.toString();
 	}
